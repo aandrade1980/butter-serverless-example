f1567fe74019262291a4c42bec1336457db7436b
alvaro-andrade <alvaro.andrade@salesforce.com>
2019-09-05 12:03:36 -0300

    Added netlify function. Updated node modules

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import fetch from "isomorphic-fetch";
+
+const indexToMonth = index => {
+  const months = {
+    0: () => "Jan",
+    8: () => "Sep",
+    11: () => "Dec",
+    default: () => ""
+  };
+
+  return months[index] || months["default"];
+};
 
 function App() {
+  const [postsByMonth, setPostsByMonth] = useState(null);
+
+  const onClick = async () => {
+    try {
+      const response = await fetch("/.netlify/functions/posts-by-month");
+      const json = await response.json();
+      setPostsByMonth(json);
+    } catch (e) {
+      console.error(e);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={onClick}>Fetch API</button>
+      {postsByMonth && (
+        <table>
+          <thead>
+            <th>Month</th>
+            <th>Posts</th>
+          </thead>
+          <tbody>
+            {Array(12)
+              .fill(0)
+              .map((el, i) => (
+                <tr key={i}>
+                  <td>{indexToMonth(i)()}</td>
+                  <td>{postsByMonth[i] || 0}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
